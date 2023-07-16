afbca4c9712be2039cd80fcce506430d76ddf931
koguretaichi <taichi.kogure1105@gmail.com>
2023-07-16 21:39:30 +0800

    Initial commit

---
+++ 2023F-Study/Pybamm202307.py
@@ -0,0 +1,45 @@
+#%%
+
+# import pybamm
+import pybamm
+model = pybamm.lithium_ion.DFN()  # Doyle-Fuller-Newman model
+sim = pybamm.Simulation(model)
+sim.solve([0, 3600])  # solve for 1 hour
+sim.plot()
+
+#%%
+#
+# GITT discharge
+#
+import pybamm
+
+pybamm.set_logging_level("INFO")
+experiment = pybamm.Experiment(
+    [("Discharge at C/20 for 1 hour", "Rest for 1 hour")] * 20,
+)
+model = pybamm.lithium_ion.DFN()
+sim = pybamm.Simulation(model, experiment=experiment, solver=pybamm.CasadiSolver())
+sim.solve()
+sim.plot()
+
+
+#%%
+
+import tensorflow as tf
+print(tf.__version__)
+mnist = tf.keras.datasets.fashion_mnist
+(training_images, training_labels), (test_images, test_labels) = mnist.load_data()
+# To experiment with removing normalization, comment out the following 2 lines
+training_images=training_images/255.0
+test_images=test_images/255.0
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Flatten(),
+  tf.keras.layers.Dense(512, activation=tf.nn.relu),
+  tf.keras.layers.Dense(10, activation=tf.nn.softmax)
+])
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy')
+model.fit(training_images, training_labels, epochs=5)
+model.evaluate(test_images, test_labels)
+classifications = model.predict(test_images)
+print(classifications[0])
+print(test_labels[0])
