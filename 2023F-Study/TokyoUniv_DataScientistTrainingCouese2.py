cd26944201bf7af1617a348c29e61435a649e6d7
koguretaichi <taichi.kogure1105@gmail.com>
2023-11-05 15:56:28 +0800

    20231105

--- 2023F-Study/TokyoUniv_DataScientistTrainingCouese2.py
+++ 2023F-Study/TokyoUniv_DataScientistTrainingCouese2.py
@@ -4,13 +4,13 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-calc_time = 1000
+calc_time = 300
 #サイコロ
 sample_array = np.array([1,2,3,4,5,6])
 number_cnt = np.arange(1, calc_time + 1)
 
 #４つのパスを生成
-for i in range(4):
+for i in range(200):
     p = np.random.choice(sample_array, calc_time).cumsum()
     plt.plot(p/ number_cnt)
     plt.grid(True)
