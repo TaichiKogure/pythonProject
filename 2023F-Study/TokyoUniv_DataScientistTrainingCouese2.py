00b0c9c408672974d037ad581a368b3e8b843ec7
koguretaichi <taichi.kogure1105@gmail.com>
2023-11-10 20:18:21 +0800

    20231110

--- 2023F-Study/TokyoUniv_DataScientistTrainingCouese2.py
+++ 2023F-Study/TokyoUniv_DataScientistTrainingCouese2.py
@@ -29,20 +29,21 @@ def function_central_theory(N):
 
     mean_array = np.array([])
 
-    for i in range(1000):
-        cum_variables = np.random.choice(sample_array, N).comsum() * 1.0
+    for i in range(100):
+        cu_variables = np.random.choice(sample_array, N)
+        cum_variables = np.cumsum(cu_variables) *1.0
         mean_array = np.append(mean_array, cum_variables[N-1] / N)
 
-    plt.hist(mean_array)
+    plt.hist(mean_array, bins=10, density=True, alpha=0.9)
     plt.grid(True)
     plt.show()
 #"動かない"
 
-function_central_theory(3)
+function_central_theory(10000)
 
 #%%
 num_simulations = 10000  # シミュレーションの回数
-num_rolls = 100000  # サイコロを振る回数
+num_rolls = 1000  # サイコロを振る回数
 
 sums = np.zeros(num_simulations)
 
@@ -50,10 +51,17 @@ for i in range(num_simulations):
     dice_rolls = np.random.randint(1, 7, num_rolls)  # サイコロをnum_rolls回振った結果
     sums[i] = np.sum(dice_rolls)  # サイコロの合計を記録
 
-plt.hist(sums, bins=30, density=True, alpha=0.4, color='b', label='Simulated Sum')
+plt.hist(sums, bins=200, density=True, alpha=0.8, color='b', label='Simulated Sum')
 plt.xlabel('Sum of Dice Rolls')
 plt.ylabel('Probability Density')
 plt.title('Central Limit Theorem Simulation')
 plt.legend()
-plt.show(
-    #%%
+plt.show()
+#%%
+#カイ二条分布
+#自由度２、１０、６０に従うかい2乗分布
+for df, c in zip([2,4,10],["#6495ed","#daa520","#ff00ff"]):
+    x = np.random.chisquare(df, 100000)
+    plt.hist(x, 50, color = c)
+plt.grid(True)
+plt.show()
