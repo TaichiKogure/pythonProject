9ac991fc836e50f655f317e861a792405aaedf77
koguretaichi <taichi.kogure1105@gmail.com>
2023-12-28 22:35:24 +0800

    Implement 3D plotting and animation in ContinuousFileReading.py
    The code now includes scripts for creating 3D scatter plots of randomly generated data and adding animations to these plots. Particularly, it contains a rotating 3D scatter plot saved as 'rotation.gif', ensuring a dynamic visualization of the data.

--- 2023F-Study/ContiniousFileReading.py
+++ 2023F-Study/ContiniousFileReading.py
@@ -40,3 +40,67 @@ for csv_file in csv_files:
 
 plt.legend()
 plt.show()
+#%%
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+# データの作成
+np.random.seed(0)
+x = np.random.randn(100)
+y = np.random.randn(100)
+z = np.random.randn(100)
+
+# 描画の準備
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+# プロット
+ax.scatter(x, y, z)
+plt.show()
+#%%
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+# データの作成
+np.random.seed(0)
+x = np.random.randn(100)
+z = np.random.randn(100)
+y = x + 2*z
+
+# 描画の準備
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+# プロット
+ax.scatter(x, y, z)
+plt.show()
+#%%
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import numpy as np
+
+# Use rc_context as a context manager
+with plt.rc_context({'animation.writer': 'pillow'}):
+
+    # データの作成
+    np.random.seed(0)
+    x = np.random.randn(1000)
+    z = np.random.randn(1000)
+    y = 3*x + 2*z
+
+    # 描画の準備
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.scatter(x, y, z)
+
+    # 回転させるための関数
+    def rotate(angle):
+        ax.view_init(azim=angle)
+
+    ani = animation.FuncAnimation(fig, rotate, frames=np.arange(0, 360, 2), interval=100)
+
+    ani.save('rotation.gif', writer='pillow', fps=20)
+
+    plt.show()
